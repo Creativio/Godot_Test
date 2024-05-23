1b4b72b63943993ad9fc74ffdf4187b61aa0235c
Creativio <koricks@mail.ru>
2024-05-23 19:37:42 +0300

    player animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,7 +2,10 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -100.0
+
+var is_attacking = false
+var is_climbing = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,6 +19,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_Space") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -25,4 +29,35 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+	#move_and_slide()
+	if !is_attacking:
+		player_animation()
+
+func _input(event):
+	if event.is_action_pressed("ui_F"):
+		is_attacking = true
+		$AnimatedSprite2D.play("attack")
+	
+	if is_climbing == true and Input.is_action_pressed("ui_up"):
+		$AnimatedSprite2D.play("climb")
+		velocity.y = -300
+	else:
+		is_climbing = false
+	
+
+#animation
+func player_animation():
+	if Input.is_action_pressed("ui_left") || Input.is_action_just_released("ui_Space"):
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("run")
+		
+	if Input.is_action_pressed("ui_right") || Input.is_action_just_released("ui_Space"):
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("run")
+	
+	if !Input.is_anything_pressed():
+		$AnimatedSprite2D.play("idle")
+
+
+func _on_animated_sprite_2d_animation_finished():
+	is_attacking = false
